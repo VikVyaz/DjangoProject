49430c04e770e884b8fb251c9630f574f08aacb8
VikVyaz <gluckundhappy@gmail.com>
2025-08-16 19:22:27 +0300

    git commit failure. fixes

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name='Email', unique=True)
+    avatar = models.ImageField(verbose_name='Аватар', upload_to='avatars/', blank=True, null=True)
+    phone_number = PhoneNumberField(verbose_name='Номер телефона', max_length=15, blank=True, null=True)
+    country = CountryField(verbose_name='Страна', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.username} - {self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователи'
+        verbose_name_plural = 'Пользователь'
+        permissions = [
+            ('view_users_list', 'Может просматривать список пользователей')
+        ]
